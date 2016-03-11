56b2511b6ae97efd6a21d4d80dc2b197f36e4e47
Bertrand Bordage <bordage.bertrand@gmail.com>
2016-03-11 19:45:38 +0100

    Adds email settings.

--- noripyt/settings/production.py
+++ noripyt/settings/production.py
@@ -5,6 +5,13 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['noripyt.com', 'dev.noripyt.com']
 
+SERVER_HOST = 'ssl0.ovh.net'
+SERVER_PORT = 587
+DEFAULT_FROM_EMAIL = 'noreply@noripyt.com'
+SERVER_EMAIL = DEFAULT_FROM_EMAIL
+EMAIL_SUBJECT_PREFIX = '[Noripyt.com] '
+EMAIL_TIMEOUT = 5
+
 try:
     from .local import *
 except ImportError:
