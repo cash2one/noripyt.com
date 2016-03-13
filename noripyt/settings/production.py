146150a72d3fe8800fe25fc67a0f4527fdd6ee10
Bertrand Bordage <bordage.bertrand@gmail.com>
2016-03-13 17:26:20 +0100

    Fixes the SMTP settings.

--- noripyt/settings/production.py
+++ noripyt/settings/production.py
@@ -3,13 +3,14 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['noripyt.com', 'dev.noripyt.com']
+ALLOWED_HOSTS = ['.noripyt.com']
 
-SERVER_HOST = 'ssl0.ovh.net'
-SERVER_PORT = 587
-DEFAULT_FROM_EMAIL = 'noreply@noripyt.com'
+EMAIL_HOST = 'mail.gandi.net'
+EMAIL_HOST_USER = 'noreply@noripyt.com'
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', '')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 SERVER_EMAIL = DEFAULT_FROM_EMAIL
-EMAIL_SUBJECT_PREFIX = '[Noripyt.com] '
+EMAIL_SUBJECT_PREFIX = '[NoriPyt.com] '
 EMAIL_TIMEOUT = 5
 
 try:
